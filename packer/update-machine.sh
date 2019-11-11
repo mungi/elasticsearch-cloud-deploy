26744354ecdf2b4be04f9139d585ed42d4f53829
mungi <will2you@gmail.com>
2019-11-11 14:27:26 +0900

    apply terraform 0.12 & remove azure

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -3,14 +3,13 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo rm /boot/grub/menu.lst
+#sudo rm /boot/grub/menu.lst
 
 sudo -E apt-get upgrade -y
-sudo -E apt-get install -y software-properties-common git python-dev htop ntp jq apt-transport-https
+sudo -E apt-get install -y software-properties-common git python-dev htop ntp jq apt-transport-https python3-pip
+pip3 install awscli boto3
+
 
 # Disable daily apt unattended updates.
-echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
+sudo bash -c "echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic"
 
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py || python2 get-pip.py
-pip install boto awscli || pip2 install boto awscli 
--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,7 +1,12 @@
 output "clients_dns" {
-  value = "${aws_elb.es_client_lb.*.dns_name}"
+  value = aws_elb.es_client_lb.*.dns_name
+}
+
+output "vm_user" {
+  value = var.client_user
 }
 
 output "vm_password" {
-  value = "${random_string.vm-login-password.result}"
+  value = random_string.vm-login-password.result
 }
+
